f1f26b4e895efaf0745189903e77e5496cc6eedc
gaoyuan <gaoyuan_flying@163.com>
2017-08-14 11:37:28 +0800

    纯canvas绘制时钟---增加比例控制，改变canvas时钟的宽度高度，不变样

--- canvas_clock/js/clock.js
+++ canvas_clock/js/clock.js
@@ -7,42 +7,44 @@
   var canvasWidth = ctx.canvas.width
   var canvasHeight = ctx.canvas.height;
   
-  var r = canvasWidth / 2;
+  var scale = canvasWidth / 200;
+  
+  var r = canvasWidth / 2 ;
   function drawBackground() {
     
     ctx.save();
     ctx.translate(r,r);
     ctx.beginPath();
   
-    ctx.lineWidth =10;
+    ctx.lineWidth =　10 * scale;
     
-    ctx.arc(0,0,r - 5,0,2*Math.PI,false);
+    ctx.arc(0, 0, r - ctx.lineWidth / 2, 0, 2 * Math.PI, false);
     ctx.stroke();
     
-    var hourNumber = [3,4,5,6,7,8,9,10,11,12,1,2];
-    ctx.font = '18px Arial';
+    var hourNumber = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
+    ctx.font = 18 * scale + 'px Arial';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     //时辰
     hourNumber.forEach(function (number, i) {
       var rad = 2 * Math.PI / 12 *i;
-      var x = Math.cos(rad)*(r-30);
-      var y = Math.sin(rad)*(r-30);
+      var x = Math.cos(rad) * (r - 30 * scale);
+      var y = Math.sin(rad) * (r - 30 * scale);
       
       ctx.fillText(number,x,y)
     })
     //60个小点
     for (var i=0;i<60;i++){
       var rad = 2*Math.PI/60*i;
-      var x = Math.cos(rad)*(r-18);
-      var y = Math.sin(rad)*(r-18);
+      var x = Math.cos(rad)*(r - 18 * scale);
+      var y = Math.sin(rad)*(r - 18 * scale);
       ctx.beginPath();
-      if(i%5 === 0){
+      if(i % 5 === 0){
         ctx.fillStyle = '#000';
       }else{
         ctx.fillStyle = '#ccc';
       }
-      ctx.arc(x,y,2,0,2*Math.PI,false);
+      ctx.arc(x, y, 2 * scale, 0, 2 * Math.PI, false);
       ctx.fill();
     }
     
@@ -56,7 +58,7 @@
     ctx.rotate(rad + minRad);
   
     ctx.moveTo(0,10);
-    ctx.lineWidth = 6;
+    ctx.lineWidth = 6 * scale;
     ctx.lineCap = 'round';
     ctx.lineTo(0,-r/2);
     ctx.stroke();
@@ -70,9 +72,9 @@
     ctx.rotate(rad);
     
     ctx.moveTo(0,15);
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 3 * scale;
     ctx.lineCap = 'round';
-    ctx.lineTo(0,-r/2 - 5)
+    ctx.lineTo(0,-r/2 - 5*scale)
     ctx.stroke();
     ctx.restore();
   }
@@ -82,7 +84,7 @@
     ctx.beginPath();
     var rad = 2 * Math.PI / 60 * second;
     ctx.rotate(rad);
-    ctx.fillStyle = 'red'
+    ctx.fillStyle = 'red';
     ctx.moveTo(-2,20);
     ctx.lineTo(2,20);
     ctx.lineTo(1,-r + 15)
@@ -95,7 +97,7 @@
   function drawDot() {
     ctx.beginPath();
     ctx.fillStyle = 'white';
-    ctx.arc(0,0,3,2*Math.PI,false)
+    ctx.arc(0,0,3*scale,2*Math.PI,false)
     ctx.fill()
   }
   
