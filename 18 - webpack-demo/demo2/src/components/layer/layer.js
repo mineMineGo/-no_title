0682d947369fc071fab8bde7be81259e12dbdc94
minemine <1372156068@qq.com>
2018-03-17 18:08:57 +0800

    webpack-demo

---
+++ 18 - webpack-demo/demo2/src/components/layer/layer.js
@@ -0,0 +1,14 @@
+/**
+ * Created by gaoyuan on 17-8-30.
+ */
+
+// import tpl from './layer.html';
+
+function　layer(){
+    return {
+        name: 'layer',
+        tpl: tpl
+    }
+}
+
+export default layer
