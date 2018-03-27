c208142f6e94e24e13ec139db05db4ae856a80f7
minemine <1372156068@qq.com>
2018-03-27 18:29:22 +0800

    使用gulp实现css的压缩以及监听

---
+++ 23 - build/index06/06-1/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+
+var cleanCss = require('gulp-clean-css');
+
+var sequence = require('run-sequence');
+
+gulp.task('default', function(){
+  sequence('minify-css','cssWatch');
+});
+
+gulp.task('minify-css', function(){
+  gulp.src('assets/css/*.css')
+    .pipe(cleanCss())
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('cssWatch', function(){
+  gulp.watch("assets/css/*.css", function(){
+    sequence('minify-css');
+  });
+});
