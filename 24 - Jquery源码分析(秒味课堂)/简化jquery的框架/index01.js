b76acc2dc96f6ff0cfeb5cd2d9a47541f63d7fc1
minemine <1372156068@qq.com>
2018-04-07 17:21:54 +0800

    access是attr,props等设置属性和获得属性的一个公共部分方法

--- 24 - Jquery源码分析(秒味课堂)/简化jquery的框架/index01.js
+++ 24 - Jquery源码分析(秒味课堂)/简化jquery的框架/index01.js
@@ -749,6 +749,56 @@
       proxy.guid = fn.guid = fn.guid || jQuery.guid++;
 
       return proxy;
+    },
+    // 多功能值操作(内部)
+    // elems可能是个集合
+    // fn是个回调函数
+    // key 就是类似设置属性的属性名字
+    // value 就是类似于设置属性的属性值
+    // chainable 如果为真就代表要设置，为假就是获取
+
+    access: function (elems, fn, key,value,chainable, emptyGet, raw) {
+      var i = 0,
+          length = elems.length,
+          bulk = key == null;
+
+    //  设置多个值
+      if(jQuery.type(key) === 'object'){
+        chainable  = true;
+        for(i in key){
+          jQuery.access(elems, fn, i, key[i], true, emptyGet, raw);
+        }
+      }else if(value !== undefined) {
+        // 设置单个值
+        chainable = true;
+
+        if(!jQuery.isFunction(value)){
+          raw = true;
+        }
+
+        if(bulk){
+          if(raw){
+            fn.call(elems, value);
+            fn = null;
+          } else {
+            bulk = fn;
+            fn = function (elem, key, value) {
+              return bulk.call(jQuery(elem), value);
+            }
+          }
+        }
+        if(fn){
+          for(;i<length;i++){
+            fn(elems[i], key, raw ? value : value.call(elems[i], i, fn(elems[i],key)) )
+          }
+        }
+      }
+      return chainable ?
+          elems:
+          // gets
+          bulk ?
+              fn.call(elems) :
+              length ? fn(elems[0], key) : emptyGet;
     }
   });
   // 844
