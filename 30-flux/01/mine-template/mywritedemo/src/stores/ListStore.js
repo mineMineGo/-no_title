1837c3c925454cb38405bc0ebc315b8ea18765cb
gaoyuan <gaoyuan_flying@163.com>
2018-11-20 18:37:23 +0800

    用箭头函数不能扎到this

--- 30-flux/01/mine-template/mywritedemo/src/stores/ListStore.js
+++ 30-flux/01/mine-template/mywritedemo/src/stores/ListStore.js
@@ -1,12 +1,13 @@
 import { EventEmitter } from "events";
+import assign from "object-assign";
 
-const ListStore = Object.assign({}, EventEmitter.prototype, {
+const ListStore = assign({}, EventEmitter.prototype, {
   items: [],
 
-  addNewItemHandler: text => {
+  addNewItemHandler: function(text) {
     console.log("store中收到了dispatch的变化");
+    console.log(this.items);
     // this.items.push(text);
-    // console.log(this.items);
   }
 });
 
